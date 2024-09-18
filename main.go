1fc22a1a9b9a7ca25b42cc502642c5b5da9f24d9
pratikshashete04 <pratikshashete04@gmail.com>
2024-09-18 15:29:01 +0530

    added code to return multiple values

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
-// simple hello prog
+// 1} simple hello prog
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello")
+// }
+
+// 2} Return multiple values from return
 package main
 
 import "fmt"
 
+func add_remove() (int, int) {
+	x := 9
+	y := 6
+	m := x + y
+	n := x - y
+	return m, n
+}
+
 func main() {
-	fmt.Println("Hello")
+	p, o := add_remove()
+	fmt.Println(p, o)
 }
